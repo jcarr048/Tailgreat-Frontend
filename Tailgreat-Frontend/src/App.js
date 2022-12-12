df9dddbafcadaa6c7d4b8f9b78b23d81b661871a
Jeff Carroll <jcarr048@gmail.com>
2022-12-12 10:21:38 -0500

    established initial routes on app.js

--- Tailgreat-Frontend/src/App.js
+++ Tailgreat-Frontend/src/App.js
@@ -1,15 +1,97 @@
 import './App.css'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import axios from 'axios'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Nav from './components/Nav'
+import { BASE_URL } from './globals'
+import {
+  LoginHost,
+  LoginUser,
+  RegisterHost,
+  RegisterUser
+} from './services/Authorize'
 
-function App() {
+const App = () => {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
+  const [host, setHost] = useState(null)
+  const [tailgateList, setTailgateList] = useState([])
+  const [feedback, setFeedback] = useState(null)
+  const [editFeedback, setEditFeedback] = useState(null)
+  const [selectedTailgate, setSelectedTailgate] = useState(null)
+
+  const getTailgates = async () => {
+    const res = await axios.get(`${BASE_URL}/tailgates`)
+    setTailgateList(res.data)
+  }
+  useEffect(() => {
+    getTailgates
+  }, [])
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
   return (
     <div className="App">
       <header>
-        <Nav />
+        <NavBar
+          authenticated={authenticated}
+          user={user}
+          host={host}
+          handleLogOut={handleLogOut}
+        />
       </header>
+      <main>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                user={user}
+                host={host}
+                authenticated={authenticated}
+                tailgateList={tailgateList}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <LoginUser
+                setUser={setUser}
+                toggleAuthenticated={toggleAuthenticated}
+              />
+            }
+          />
+          <Route
+            path="/loginHost"
+            element={
+              <LoginHost
+                setHost={setHost}
+                toggleAuthenticated={toggleAuthenticated}
+              />
+            }
+          />
+          <Route
+            path="/tailgate/:id"
+            element={
+              <TailgateDetails
+                selectedTailgate={selectedTailgate}
+                authenticated={authenticated}
+                user={user}
+                host={host}
+              />
+            }
+          />
+          <Route path="/registerUser" element={<RegisterUser />} />
+          <Route path="registerHost" element={<RegisterHost />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </main>
     </div>
   )
 }
