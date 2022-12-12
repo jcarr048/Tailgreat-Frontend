8f92ef5210f48de3b1c2b5413356e1c35cefb316
Jeff Carroll <jcarr048@gmail.com>
2022-12-12 09:28:13 -0500

    added a few more pages for host options

--- Tailgreat-Frontend/src/App.js
+++ Tailgreat-Frontend/src/App.js
@@ -1,12 +1,17 @@
-import './index.css';
-import Test from './components/TestComponent';
+import './index.css'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { useEffect } from 'react'
+import Nav from './components/Nav'
 
 function App() {
   return (
-    <div>
-      <Test />
+    <div className="App">
+      <header>
+        <Nav />
+      </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- Tailgreat-Frontend/src/components/Nav.jsx
+++ Tailgreat-Frontend/src/components/Nav.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import { NavLink } from "react-router-dom"
 
-const TestComponent = () => {
+const NavBar = ({}) => {
   return (
     <>
       <h1>TestComponent</h1>
