5287f61701c8dfc2d8308b6afa0246b0cfd762a4
Jeff Carroll <jcarr048@gmail.com>
2023-01-05 13:13:06 -0500

    baseurl trials continue

--- src/globals.js
+++ src/globals.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'https://carroll-tailgreat-backend-production.up.railway.app/'
+const BASE_URL =
+  'https://carroll-tailgreat-backend-production.up.railway.app/api'
 
 module.exports = {
   BASE_URL
